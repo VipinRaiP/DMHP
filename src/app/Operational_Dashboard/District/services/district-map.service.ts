1d3f9e4543678f304a21e46819ad3ed14e0e4312
Harshabh Mahant <harshabhmahant11@gmail.com>
2020-04-14 15:46:21 +0530

    a

--- src/app/Operational_Dashboard/District/services/district-map.service.ts
+++ src/app/Operational_Dashboard/District/services/district-map.service.ts
@@ -1,6 +1,7 @@
 import { EventEmitter } from '@angular/core';
 
 export class DistrictMapService {
-    onDistrictSelected = new EventEmitter<any>();
+    onDistrictSelected = new EventEmitter<any>(); //hover
     onYearChanged = new EventEmitter<number>();
+    onDistrictClicked = new EventEmitter<any>();
 }
