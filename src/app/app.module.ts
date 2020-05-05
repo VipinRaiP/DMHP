d6b1787c75bab682b54e08a89383bf334c417768
VipinRaiP <vipin.rai.p@gmail.com>
2020-05-05 19:44:42 +0530

    Minor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -121,7 +121,7 @@ export function tokenGetter() {
   ],
   providers: [
     LineChartService,
-    PatientCountService,
+    //PatientCountService,
     PatientCountDistrictService,
     PatientCountTalukaService,
     ExpenseCountDistrictService,
